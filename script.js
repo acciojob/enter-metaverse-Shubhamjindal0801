441acbd72ee88400b3844d09b152805e1cc7e32a
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-13 07:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function meta(){
+            document.getElementById("status").style.fontSize = "2rem";
+            document.getElementById("status").style.fontWeight = "bold";
+        }
