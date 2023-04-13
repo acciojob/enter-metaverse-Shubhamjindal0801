99a4e7ffdacb61c6936a0ee9fef21db7066f6034
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-13 13:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 function meta(){
+	document.getElementById("status").innerHTML = "Entered Metaverse";
             document.getElementById("status").style.fontSize = "2rem";
             document.getElementById("status").style.fontWeight = "bold";
         }
