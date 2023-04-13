8e9b019775b7e5e80841589d0475252f5a8fa1a1
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-13 13:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //your JS code here. If required.
 function meta(){
-	document.getElementById("status").innerHTML = "Entered Metaverse";
-            document.getElementById("status").style.fontSize = "2rem";
-            document.getElementById("status").style.fontWeight = "bold";
+	const pElement = document.getElementById("status");
+
+// Create a new h1 element
+const h1Element = document.createElement('h1');
+
+// Copy the text content from the p element to the h1 element
+h1Element.textContent = "Entered Metaverse"
+
+// Replace the p element with the new h1 element
+pElement.replaceWith(h1Element);
         }
